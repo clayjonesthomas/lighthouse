377fef0121ff61d4e49b4eaef8091a0582787e71
clay <clayjonesthomas@gmail.com>
2016-12-27 19:01:36 -0600

    updated index.js to point to the new form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
+import { createStore } from 'redux'
+import lighthouse from '../reducers/reducer.js'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import NewPostHandler from 'containers/NewPostHandler'
+import {Provider} from 'react-redux'
+let store = createStore(lighthouse)
 
+import './index.css';
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <NewPostHandler />
+  </Provider>,
   document.getElementById('root')
 );
