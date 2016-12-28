de0466b9c35e1059cbf26044ec1012a215153e2f
clay <clayjonesthomas@gmail.com>
2016-12-27 19:01:08 -0600

    constant-ized action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,26 @@
+export const ADD_POST = 'ADD_POST'
+export const CANCEL_POST = 'CANCEL_POST'
+export const UPDATE_FORM = 'UPDATE_FORM'
+
+
 let nextPostId = 0
 export const addPost = (post) => {
   return {
-    type: 'ADD_POST',
-    id: nextPostId++
+    type: ADD_POST,
+    id: nextPostId++,
+    post
   }
 }
 
 export const cancelPost = () => {
   return {
-    type: 'CANCEL_POST'
+    type: CANCEL_POST
   }
 }
 
 export const updateForm = (key, value) => {
   return {
-    type: 'UPDATE_FORM',
+    type: UPDATE_FORM,
     key,
     value
   }
