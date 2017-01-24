3d8029ff69178c676d63720b16db5fe25fd632cb
clay <clayjonesthomas@gmail.com>
2017-01-24 15:16:00 -0500

    updated reducer for auth

--- src/web/reducers/reducer.js
+++ src/web/reducers/reducer.js
@@ -1,8 +1,10 @@
 import {ADD_POST, CANCEL_POST} from '../actions/NewPostActions.js'
 import {REQUEST_POSTS_RETURN} from '../actions/FrontPageActions'
+import {RESPONSE_AUTH} from '../actions/AuthActions.js'
 
 const initialState = {
-  displayedPosts: []
+  displayedPosts: [],
+  jwt: null
 }
 
 function lighthouse(state = initialState, action) {
@@ -15,6 +17,10 @@ function lighthouse(state = initialState, action) {
       return state
     case CANCEL_POST:
       return state
+    case RESPONSE_AUTH:
+      return Object.assign({}, state, {
+        jwt: action.data.jwt
+      })
     default:
       return state
   }
