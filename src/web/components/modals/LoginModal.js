4beddd812a6ff749cdea10b405f6718ff5eb92d3
clay <clayjonesthomas@gmail.com>
2017-03-28 13:14:51 -0400

    adding bootstrap to the UI, starting with TextBox

--- src/web/components/modals/LoginModal.js
+++ src/web/components/modals/LoginModal.js
@@ -1,11 +1,11 @@
 import React, {PropTypes} from 'react'
 import TextBox from '../ui-kit/TextBox'
 import SubmitButton from '../ui-kit/SubmitButton'
-import CancelButton from '../CancelButton'
 import {Link} from 'react-router'
 import './LoginModal.css'
 
 import Modal from 'react-modal'
+import {Col, FormGroup} from 'react-bootstrap'
 
 const LOGIN_USERNAME = "LOGIN_USERNAME"
 const LOGIN_PASSWORD = "LOGIN_PASSWORD"
@@ -31,37 +31,44 @@ const LoginModal = ({onCancel, onLogin, onSignUp}) => (
     style={LoginStyle}
   >
     <p className='auth-title'> Login or Sign Up </p>
-    <div className='login'>
-      <TextBox
-        idName={LOGIN_USERNAME}
-      />
-      <br/>
-      <TextBox
-        idName={LOGIN_PASSWORD}
-      />
-      <br/>
-      <SubmitButton
-        onClick={() => onLogin(LOGIN_USERNAME, LOGIN_PASSWORD)}
-      />
-    </div>
-    <div className='sign-up'>
-      <TextBox
-        idName={SIGN_UP_USERNAME}
-      />
-      <br/>
-      <TextBox
-        idName={SIGN_UP_PASSWORD_1}
-      />
-      <br/>
-      <TextBox
-        idName={SIGN_UP_PASSWORD_2}
-      />
-      <br/>
-      <SubmitButton
-        onClick={ () =>
-          onSignUp(SIGN_UP_USERNAME, SIGN_UP_PASSWORD_1, SIGN_UP_PASSWORD_2)}
-      />
-    </div>
+    <FormGroup>
+      <Col componentClass='login' sm={6}>
+        <TextBox
+          idName={LOGIN_USERNAME}
+          placeholder="username"
+        />
+        <br/>
+        <TextBox
+          idName={LOGIN_PASSWORD}
+          placeholder="password"
+        />
+        <br/>
+        <SubmitButton
+          onClick={() => onLogin(LOGIN_USERNAME, LOGIN_PASSWORD)}
+        />
+      </Col>
+      <Col componentClass='sign-up' sm={6}>
+        <TextBox
+          idName={SIGN_UP_USERNAME}
+          placeholder="choose a username"
+        />
+        <br/>
+        <TextBox
+          idName={SIGN_UP_PASSWORD_1}
+          placeholder="enter a password"
+        />
+        <br/>
+        <TextBox
+          idName={SIGN_UP_PASSWORD_2}
+          placeholder="confirm your password"
+        />
+        <br/>
+        <SubmitButton
+          onClick={ () =>
+            onSignUp(SIGN_UP_USERNAME, SIGN_UP_PASSWORD_1, SIGN_UP_PASSWORD_2)}
+        />
+      </Col>
+    </FormGroup>
   </Modal>
 )
 
--- src/web/components/ui-kit/TextBox.js
+++ src/web/components/ui-kit/TextBox.js
@@ -1,12 +1,21 @@
 import React, {PropTypes} from 'react'
 import './TextBox.css'
+import {FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
 
-const TextBox = ({idName}) => (
-  <input
-    type="text"
-    id={idName}
-    className="text-box">
-  </input>
+const TextBox = ({idName, placeholder, label, className}) => (
+  <FormGroup controlId={idName}>
+    <ControlLabel>{label}</ControlLabel>
+    <FormControl
+      type="text"
+      placeholder={placeholder}
+      className={"text-box"+className}
+    />
+  </FormGroup>
+  // <input
+  //   type="text"
+  //   id={idName}
+  //   className="text-box">
+  // </input>
 )
 
 TextBox.propTypes = {
