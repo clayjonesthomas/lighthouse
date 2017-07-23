668ff997f2a007e876d9297854f165566141825c
clay <clayjonesthomas@gmail.com>
2017-07-23 19:53:40 -0400

    finished preliminary scaffold

--- src/web/components/MyShopsPage.js
+++ src/web/components/MyShopsPage.js
@@ -3,6 +3,7 @@ import React, {PropTypes} from 'react'
 import Spinner from './ui-kit/Spinner'
 import ShopFinder from './ui-kit/ShopFinder'
 import SubmitButton from './ui-kit/SubmitButton'
+import {Grid, Col, Row} from 'react-bootstrap'
 
 const MyShopsPage =
   ({
@@ -20,34 +21,46 @@ const MyShopsPage =
       {!areShopsLoaded &&
         <Spinner/>
       }
-      {areShopsLoaded &&
-      <ShopFinder
-        shops={shops}
-        onAddNewShop={(shop) => {
-          onAddNewShop(shop)
-        }}
-        onAddShopFinderRef={ref => onAddShopFinderRef(ref)}
-        placeholder="search for a shop..."
-      />
-      }
-      {areShopsLoaded &&
-        <SubmitButton
-          onClick={() => {
-            onSubmitShops()
-            clearShopFinder()
-          }
-        }
-        />
-      }
-      {areShopsLoaded && myShops &&
-        myShops.map(shop => {
-          return <ShopBox
-            shop={shop}
-            key={shop.key}
-            onLike={() => onLike(shop.key)}
-          />
-        })
-      }
+      <Grid>
+        <Row>
+          <Col md={8}>
+            {areShopsLoaded && myShops &&
+              myShops.map(shop => {
+                return <ShopBox
+                  shop={shop}
+                  key={shop.key}
+                  onLike={() => onLike(shop.key)}
+                />
+              })
+            }
+            {areShopsLoaded && !myShops &&
+              "You're currently not following any shops. Go add some! -->"
+            }
+          </Col>
+          <Col md={2}>
+            {areShopsLoaded &&
+            <ShopFinder
+              shops={shops}
+              onAddNewShop={(shop) => {
+                onAddNewShop(shop)
+              }}
+              onAddShopFinderRef={ref => onAddShopFinderRef(ref)}
+              placeholder="search for a shop..."
+            />
+            }
+            {areShopsLoaded &&
+              <SubmitButton
+                onClick={
+                  () => {
+                    onSubmitShops()
+                    clearShopFinder()
+                  }
+                }
+              />
+            }
+          </Col>
+        </Row>
+      </Grid>
     </div>
   )
 
