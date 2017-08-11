21d45457662f877f684bb04c1e4b8e8bf30aa690
clay <clayjonesthomas@gmail.com>
2017-08-11 10:50:46 -0400

    now Ive really finished the my shops page

--- src/web/components/ShopBox.js
+++ src/web/components/ShopBox.js
@@ -2,37 +2,43 @@ import React, {PropTypes} from 'react'
 import LikeButton from './ui-kit/LikeButton/LikeButton'
 import {Link} from 'react-router'
 
+import "./PostBox.css"
 import "./ShopBox.css"
 const ShopBox = (
   {
     shop,
     onLike
   }) => (
-  <div>
-    <Link
-      to={"/store/"+shop.key}
-      className="shop-name">
-      {
-        shop.name
-      }
-    </Link>
-    <a
-      href={"http://" + shop.website}
-      className="go-to-site-link"
-    >
-      Go to site...
-    </a>
-    <div className="shop-options">
-      {
-        <LikeButton
-          onClick={() => onLike()}
-          isPressed={shop.isLiked}
-          likes={shop.likes}
-          className="shop-like"
-          areLikesLeft={true}
-        />
-      }
+  <div className="shop-box">
+    <div className="shop-box-inner">
+      <div className="shop-title-bar">
+        <Link
+          to={"/store/"+shop.key}
+          className="shop-name">
+          {
+            shop.name
+          }
+        </Link>
+        <a
+          href={"http://" + shop.website}
+          className="go-to-site-link"
+        >
+          Go to site...
+        </a>
+      </div>
+      <div className="shop-options clearfix">
+        {
+          <LikeButton
+            onClick={() => onLike()}
+            isPressed={shop.isLiked}
+            likes={shop.likes}
+            className="shop-like"
+            areLikesLeft={true}
+          />
+        }
+      </div>
     </div>
+    <hr className="post-line-break"/>
   </div>
 )
 
