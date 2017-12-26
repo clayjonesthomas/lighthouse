eefcaa6b5e9e1348f8f72e1e59df7ec6272f8bd1
clay <clayjonesthomas@gmail.com>
2017-12-26 15:38:01 -0500

    added the necessary structure for designing the frontend of the v2

---
+++ src/email_web/FrontPage.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const FrontPage =
+  ({}) => (<div>.</div>)
+
+export default FrontPage
---
+++ src/email_web/Root.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {createStore, applyMiddleware} from 'redux'
+import {Provider} from 'react-redux'
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+import {routerMiddleware} from 'react-router-redux'
+import thunkMiddleware from 'redux-thunk'
+
+import reducer from './reducer'
+import FrontPage from './FrontPage'
+import App from './App'
+
+let store = createStore(
+  reducer,
+  applyMiddleware(
+    thunkMiddleware,
+    routerMiddleware(browserHistory)
+  )
+)
+
+const Root = () => (
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={FrontPage}/>
+      </Route>
+    </Router>
+  </Provider>
+)
+
+export default Root
