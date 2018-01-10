401075706d81ae22b16a067d27c3068ca0fe3063
clay <clayjonesthomas@gmail.com>
2018-01-10 15:56:38 -0500

    beginning work on page 2 design

--- src/email_web/option_1b/PickStore.js
+++ src/email_web/option_1b/PickStore.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import {Typeahead} from 'react-bootstrap-typeahead'
+import {FormGroup, ControlLabel, FormControl} from 'react-bootstrap'
 
+import "./PickStore.css"
 const PickStorePage =
   ({}) => (
-    <div>.</div>
+    <div id="form-wrapper">
+      .
+    </div>
+    // {/*<form>*/}
+    //   {/*<FormGroup*/}
+    //     {/*controlId="formBasicText"*/}
+    //     {/*// validationState={this.getValidationState()}*/}
+    //   {/*>*/}
+    //     {/*<ControlLabel>*/}
+    //       {/*Working example with validation*/}
+    //     {/*</ControlLabel>*/}
+    //     {/*<FormControl*/}
+    //       {/*type="text"*/}
+    //       {/*// value={this.state.value}*/}
+    //       {/*placeholder="Enter text"*/}
+    //       {/*// onChange={this.handleChange}*/}
+    //     {/*/>*/}
+    //     {/*<FormControl.Feedback />*/}
+    //     {/*/!*<HelpBlock>Validation is based on string length.</HelpBlock>*!/*/}
+    //   {/*</FormGroup>*/}
+    // {/*</form>*/}
   )
 
 export default PickStorePage
