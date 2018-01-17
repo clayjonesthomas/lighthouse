65ad17235db0ca6a0bd033d7d420d71fca0812d7
clay <clayjonesthomas@gmail.com>
2018-01-16 20:06:12 -0500

    fixed most of the issues with displaying everything

--- src/email_web/components/SignUpPage/SignUpPage.js
+++ src/email_web/components/SignUpPage/SignUpPage.js
@@ -11,44 +11,46 @@ const SignUpPage = ({
                 }) => (
   <form className={shouldDisplay ? "form-contents" :
     "form-contents form-contents-hidden"}>
-    <div id="form-wrapper">
-      <h1 id="form-title">
-        Sign Up
-      </h1>
-      <p id="sign-up-helper-text">
-        Already have an account? <a>Sign in</a>
-      </p>
-      <FormGroup
-        // validationState={this.getValidationState()}
-      >
-        <ShopPicker
-          className="shop-picker-box"
+    <div id="wrap">
+      <div id="form-wrapper">
+        <h1 id="form-title">
+          Sign Up
+        </h1>
+        <p id="sign-up-helper-text">
+          Already have an account? <a>Sign in</a>
+        </p>
+        <FormGroup
+          // validationState={this.getValidationState()}
+        >
+          <ShopPicker
+            className="shop-picker-box"
+          />
+          <HelpBlock id="store-helper-text">
+            We recommend picking 5-6 of your favorite stores and brands to start off.
+          </HelpBlock>
+          <FormControl
+            className="form-box"
+            id="email-box"
+            type="text"
+            // value={this.state.value}
+            placeholder="Email"
+            // onChange={this.handleChange}
+          />
+          <FormControl
+            className="form-box"
+            type="text"
+            // value={this.state.value}
+            placeholder="Password"
+            // onChange={this.handleChange}
+          />
+        </FormGroup>
+        <input
+          type="submit"
+          value="TRY IT OUT"
+          className="submit-button"
+          id="form-button"
         />
-        <HelpBlock id="store-helper-text">
-          We recommend picking 5-6 of your favorite stores and brands to start off.
-        </HelpBlock>
-        <FormControl
-          className="form-box"
-          id="email-box"
-          type="text"
-          // value={this.state.value}
-          placeholder="Email"
-          // onChange={this.handleChange}
-        />
-        <FormControl
-          className="form-box"
-          type="text"
-          // value={this.state.value}
-          placeholder="Password"
-          // onChange={this.handleChange}
-        />
-      </FormGroup>
-      <input
-        type="submit"
-        value="TRY IT OUT"
-        className="submit-button"
-        id="form-button"
-      />
+      </div>
     </div>
   </form>
 )
