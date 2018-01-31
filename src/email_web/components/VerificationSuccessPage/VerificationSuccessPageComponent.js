de9dc249783c8b5bdeb127463eb82a75fa96c10a
Michelle Lauer <mflauer@mit.edu>
2018-01-30 22:38:09 -0500

    keepin it stylish

--- src/email_web/components/VerificationSuccessPage/VerificationSuccessPageComponent.js
+++ src/email_web/components/VerificationSuccessPage/VerificationSuccessPageComponent.js
@@ -3,9 +3,12 @@ import React, {PropTypes} from 'react'
 import "./VerificationSuccessPageComponent.css"
 const VerificationSuccessPageComponent =
   ({}) => (
-  <div className="verification_success_container">
-    <h2>Your account has been verified!</h2>
-    <p>You will be automatically redirected to your settings page</p>
+  <div 
+    className="form-wrapper"
+    id="verification-form-wrapper"
+  >
+    <h1>Your account has been verified!</h1>
+    <p className="helper-text">Redirecting you to your settings page...</p>
   </div>
 )
 
