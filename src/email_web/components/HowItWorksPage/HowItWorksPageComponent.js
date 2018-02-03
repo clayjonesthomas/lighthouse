746c4456aff70716527f39b97f5c8aaf2b573091
Michelle Lauer <mflauer@mit.edu>
2018-02-03 10:06:50 -0500

    how it works words

--- src/email_web/components/HowItWorksPage/HowItWorksPageComponent.js
+++ src/email_web/components/HowItWorksPage/HowItWorksPageComponent.js
@@ -4,8 +4,28 @@ import "./HowItWorksPageComponent.css"
 
 const HowtItWorksPageComponent =
   ({}) => (
-  <div>
-    <h1>DIS HOW IT WORKS</h1>
+  <div id="how-it-works-container">
+    <h1 
+      id="how-it-works-title"
+      className="form-title"
+    >
+      How It Works
+    </h1>
+    <p className="how-it-works-section">
+      You tell us which stores you care about, and we’ll send you email notifications 
+      for the sales we know you’ll like.
+    </p>
+    <p className="how-it-works-section">
+      We won’t spam you with emails about every small sale. Instead, we'll tell you about 
+      deals that are really good and  deals that don't happen often.
+    </p>
+    <p className="how-it-works-section">
+      If you decide that you want to see more, you can opt for a daily email
+      with all sales.
+    </p>
+    <p className="how-it-works-section">
+      Still have questions? <a href="mailto:info@lightho.us">Email our team!</a>
+    </p>
   </div>
 )
 
