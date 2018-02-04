b4a91b0691959cea5b4174c5d7442dae1fd0169f
Michelle Lauer <mflauer@mit.edu>
2018-02-04 14:11:10 -0500

    hamburger dropdown, signup login page, but breaks shoppicker styling

--- src/email_web/components/NavBar/NavBar.js
+++ src/email_web/components/NavBar/NavBar.js
@@ -12,29 +12,32 @@ const NavBar = ({
                   onClickLogout,
                   email
                 }) => (
-  <div id="nav-bar">
-    {isDisplayLogo &&
-      <LogoCircle
-        onClick={onClickLogo}
-        scale={.5}
-      />
-    }
-    <div id="nav-bar-link-container">
-      {email ?
-        <div id="user-nav-bar">
-          <span className="nav-bar-span">{"Signed in as " + email}</span>
-          <a className="nav-bar-link" onClick={onClickSettings} tabIndex="0">Settings</a>
-          <a className="nav-bar-link" onClick={onClickLogout} tabIndex="0">Log Out</a>
-        </div>
-        :
-        <div id="general-nav-bar">
-          <a className="nav-bar-link" onClick={onClickHowItWorks} tabIndex="0">How It Works</a>
-          <a className="nav-bar-link" onClick={onClickSignUp} tabIndex="0">Sign Up</a>
-          <a className="nav-bar-link" onClick={onClickLogIn} tabIndex="0">Log In</a>
-        </div>
+  <nav id="nav-bar" className="navbar navbar-toggleable-md navbar-inverse bg-faded">
+    <button 
+      className="navbar-toggler navbar-toggler-right" 
+      type="button" data-toggle="collapse" 
+      data-target="#nav-bar-link-container" 
+      aria-controls="nav-bar-link-container" 
+      aria-expanded="false" 
+      aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <a className="mr-auto" href="#">
+      {isDisplayLogo &&
+        <LogoCircle
+          onClick={onClickLogo}
+          scale={.5}
+        />
       }
+    </a>
+    <div className="collapse navbar-collapse" id="nav-bar-link-container">
+      <ul className="navbar-nav ml-auto dropdown">
+        <li><a className="nav-item nav-link nav-bar-link" href="#" onClick={onClickHowItWorks} tabIndex="0">How It Works</a></li>
+        <li><a className="nav-item nav-link nav-bar-link" href="#" onClick={onClickSignUp} tabIndex="0">Sign Up</a></li>
+        <li><a className="nav-item nav-link nav-bar-link" href="#" onClick={onClickLogIn} tabIndex="0">Log In</a></li>
+      </ul>
     </div>
-  </div>
+  </nav>   
 )
 
 export default NavBar
