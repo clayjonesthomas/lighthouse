59369a19bb3d6667e2365a5352769c0f1c2bf397
Michelle Lauer <mflauer@mit.edu>
2018-02-05 09:11:59 -0500

    updated how it works text

--- src/email_web/components/HowItWorksPage/HowItWorksPage.js
+++ src/email_web/components/HowItWorksPage/HowItWorksPage.js
@@ -18,12 +18,12 @@ const HowItWorksPage =
         for the sales we know you’ll like.
       </p>
       <p className="how-it-works-section">
-        We won’t spam you with emails about every small sale. Instead, we'll tell you about 
-        deals that are really good and  deals that don't happen often.
+        We won’t crowd your inbox with every little sale. Instead, we'll send you the sales we think are rare 
+        or exceptional, so you don’t have to worry about wasting your time on the small stuff.
       </p>
       <p className="how-it-works-section">
         If you decide that you want to see more, you can opt for a daily email
-        with all sales.
+        with all the updates we can find.
       </p>
       <p className="how-it-works-section">
         Still have questions? <a href="mailto:info@lightho.us" target="_top">Email our team!</a>
