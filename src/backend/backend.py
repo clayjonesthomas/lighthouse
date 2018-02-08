7047b62007ef29274bd71ec4e3de34a6f966f496
clay <clayjonesthomas@gmail.com>
2018-02-08 13:05:49 -0500

    removing script

--- src/backend/backend.py
+++ src/backend/backend.py
@@ -25,7 +25,6 @@ from google.appengine.ext import deferred
 from models import Post, Shop, User, get_entity_from_url_key
 from email import send_emails, send_verification_email, send_forgot_password_email
 import enums.EmailFrequency as EmailFrequency
-from migration_script import migration_script
 
 from google.appengine.api import app_identity, mail
 import lib.cloudstorage as gcs
@@ -259,9 +258,6 @@ class BaseHandler(webapp2.RequestHandler):
             self.session_store.save_sessions(self.response)
 
 
-has_script_run = False
-
-
 class MainPage(BaseHandler):
 
     def get(self, *args):
@@ -270,11 +266,6 @@ class MainPage(BaseHandler):
             if not User.query().fetch(1):
                 populate_dummy_datastore()
                 time.sleep(2)  # hack to prevent this from running more than once
-
-        global has_script_run
-        if not has_script_run:
-            has_script_run = True
-            migration_script()
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render())
