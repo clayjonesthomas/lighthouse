8f8c318ad7f88665578a9d90fea95b3308f63f7e
clay <clayjonesthomas@gmail.com>
2018-02-11 18:04:07 -0500

    reducer structure

--- src/email_web/components/admin/adminReducer.js
+++ src/email_web/components/admin/adminReducer.js
@@ -1,3 +1,6 @@
+import {ADMIN_POST_TITLE_CHANGE, ADMIN_PICKED_SHOPS_CHANGE,
+ADMIN_IS_IMPORTANT_CHANGE, NEW_POST_RESPONSE}
+  from './AdminActions'
 
 const defaultAdminState = {
   postTitleValue: '',
@@ -7,6 +10,20 @@ const defaultAdminState = {
 
 export function admin(state = defaultAdminState, action) {
   switch (action.type) {
+    case ADMIN_POST_TITLE_CHANGE:
+      return Object.assign({}, state, {
+        postTitleValue: action.data
+      })
+    case ADMIN_PICKED_SHOPS_CHANGE:
+      return Object.assign({}, state, {
+        selectedShops: action.data
+      })
+    case ADMIN_IS_IMPORTANT_CHANGE:
+      return Object.assign({}, state, {
+        isImportant: action.data
+      })
+    case NEW_POST_RESPONSE:
+      return defaultAdminState
     default:
       return state
   }
