e9951cf78e6add676d89573327a3053d5d8b986b
Michelle Lauer <mflauer@mit.edu>
2018-02-20 17:00:54 -0500

    byebye scripts episode 2

--- src/backend/scripts.py
+++ src/backend/scripts.py
@@ -1,6 +1,6 @@
 import csv
 import re
-from models import Shop, User
+from models import Shop
 
 
 def update_stores():
@@ -19,17 +19,4 @@ def update_stores():
             shop.put()
             count += 1
     return count
-
-def make_emails_lower():
-	for user in User.query():
-		user.email_address = user.email_address.lower()
-		user.put()
-
-def make_auth_ids_lower():
-	for user in User.query():
-		lowered_auth_ids = []
-		for auth_id in user.auth_ids:
-			lowered_auth_ids.append(auth_id.lower())
-		user.auth_ids = lowered_auth_ids
-		user.put()
 
