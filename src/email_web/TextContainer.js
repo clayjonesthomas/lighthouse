23607c2164b50d343f139324f5f5f39142de58d7
Michelle Lauer <mflauer@mit.edu>
2018-02-27 19:44:33 -0500

    login page goes to user feed

--- src/email_web/TextContainer.js
+++ src/email_web/TextContainer.js
@@ -12,7 +12,7 @@ import {logOut, pullUserEmail} from './services/UserActions'
 
 
 import {SIGN_UP_PAGE_URL, LOG_IN_PAGE_URL, SETTINGS_PAGE_URL,
-  LANDING_PAGE_URL, HOW_IT_WORKS_PAGE_URL, WELCOME_PAGE_URL} from './urls'
+  LANDING_PAGE_URL, HOW_IT_WORKS_PAGE_URL, USER_FEED_PAGE_URL} from './urls'
 
 import {WELCOME_PAGE} from './components/WelcomePage/WelcomePage'
 import {HOW_IT_WORKS_PAGE} from './components/HowItWorksPage/HowItWorksPage'
@@ -34,6 +34,7 @@ class TextContainer extends Component {
       goToLogIn,
       goToHowItWorks,
       goToSettings,
+      goToUserFeed,
       logOut,
       email
     } = this.props
@@ -42,7 +43,7 @@ class TextContainer extends Component {
         className={"container-settings text-page-container"}>
         <NavBar
           isDisplayLogo={true}
-          onClickLogo={email ? goToSettings : goToHome}
+          onClickLogo={email ? goToUserFeed : goToHome}
           onClickSignUp={goToSignUp}
           onClickLogIn={goToLogIn}
           onClickHowItWorks={goToHowItWorks}
@@ -81,6 +82,7 @@ const mapDispatchToProps = (dispatch) => {
     goToLogIn: () => dispatch(push(LOG_IN_PAGE_URL)),
     goToHowItWorks: () => dispatch(push(HOW_IT_WORKS_PAGE_URL)),
     goToSettings: () => dispatch(push(SETTINGS_PAGE_URL)),
+    goToUserFeed: () => dispatch(push(USER_FEED_PAGE_URL)),
     logOut: () => {
       dispatch(logOut())
     },
