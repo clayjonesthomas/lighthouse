abe1bc28a8d9be4a085a8879e25d5177de0f964d
Michelle Lauer <mflauer@mit.edu>
2018-04-02 15:08:36 -0400

    less fucky tabbing

--- src/email_web/components/admin/NewShopPage/NewShopComponent.js
+++ src/email_web/components/admin/NewShopPage/NewShopComponent.js
@@ -13,44 +13,44 @@ const NewShopComponent =
      submitNewShop
    }) => (
     <form action={uploadUrl}
-        encType="multipart/form-data"
-        method="post">
+      encType="multipart/form-data"
+      method="post"
+    >
       <div>
-          <a href="/admin/tracked_shops">Tracked Shops</a>
-        </div>
-        <p>Shop Name</p>
-        <input
-          id="admin-post-title"
-          name="shop-name"
-          type="text"
-          onChange={onShopNameChange}
-          value={shopNameValue}
-        />
-        <p>Shop Alt names (just comma separated please, no spaces)
-        </p>
-        <textarea
-          id="admin-post-title"
-          name="shop-alt-names"
-          type="text"
-          onChange={onShopAltNamesChange}
-          value={shopAltNamesValue}
-        />
-        <p>Shop full website</p>
-        <input
-          id="admin-post-title"
-          name="shop-site"
-          type="text"
-          onChange={onShopSiteChange}
-          value={shopSiteValue}
-        />
+        <a href="/admin/tracked_shops">Tracked Shops</a>
+      </div>
+      <p>Shop Name</p>
+      <input
+        id="admin-post-title"
+        name="shop-name"
+        type="text"
+        onChange={onShopNameChange}
+        value={shopNameValue}
+      />
+      <p>Shop Alt names (just comma separated please, no spaces)</p>
+      <textarea
+        id="admin-post-title"
+        name="shop-alt-names"
+        type="text"
+        onChange={onShopAltNamesChange}
+        value={shopAltNamesValue}
+      />
+      <p>Shop full website</p>
+      <input
+        id="admin-post-title"
+        name="shop-site"
+        type="text"
+        onChange={onShopSiteChange}
+        value={shopSiteValue}
+      />
       <div>
         <input type="file" name="img"/>
       </div>
       <input
-          id="admin-submit-button"
-          type="submit"
-          value="submit shop"
-        />
+        id="admin-submit-button"
+        type="submit"
+        value="submit shop"
+      />
   </form>
 )
 
